0200a9f1b83290259a217b160fed0db0bf8c0a82
jovan <jovangolic19@gmail.com>
2025-04-23 14:33:31 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:'/PortfolioSite/',
-  plugins: [react()]
+  plugins: [react()],
+  base:'/PortfolioSite/'
  
 });
